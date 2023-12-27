81b81d194101e3b898f2e503dbf9525f7a8da5cc
Ankita Kumari <ankitak7098@gmail.com>
2023-12-27 16:36:39 +0530

    Object

---
+++ 1. Beginners/02. Primitives/Object/Object.js
@@ -0,0 +1,11 @@
+let person = {
+    firstName : "Rohan",
+    lastName : "Kumar",
+    age : 40,
+    living : {
+        'city' : 'Bettiah',
+    }
+};
+
+console.log(person.firstName)
+console.log(person.living)
